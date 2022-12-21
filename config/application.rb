b38494509e43be279ac4ec8826c234482343e315
Katsushi OUGI <ougi@to-r.net>
2022-12-21 21:36:42 +0900

    コンフィグ修正

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,20 @@ module Myapp
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
+    config.time_zone = "Tokyo"
+    # ロケールファイルのロードパスの設定
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}").to_s]
+    config.i18n.default_locale = :ja
+
+    # ジェネレータによるソースコードの生成をOFF
+    config.generators do |g|
+      g.skip_routes true
+      g.helper false
+      g.assets false
+      # テストフレームワークをrspecに変更
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
